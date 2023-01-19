3d300bb42e56ade8e8bf98aa820302e9c3add8bd
Aaron Conaway <40375373+aconaway1@users.noreply.github.com>
2023-01-19 17:08:46 -0500

    Initial commit
    alfjsd

---
+++ main.py
@@ -0,0 +1,33 @@
+from mastodon import Mastodon
+import yaml
+from pprint import pprint
+import sys
+
+STRING_TO_FIND = sys.argv[1]
+SEPARATOR = "-" * 30
+
+with open("creds.yml") as file:
+    creds_data = yaml.safe_load(file)
+    
+masto_conn = Mastodon(access_token=creds_data['token'], api_base_url=creds_data['base_url'])
+
+me_info = masto_conn.me()
+
+my_id = me_info['id']
+my_name = me_info['display_name']
+
+my_statuses = masto_conn.account_statuses(my_id, limit=100)
+
+print(f"My Name: {my_name}  My ID: {my_id}")
+for status in my_statuses:
+    if status['reblog']:
+        # print(SEPARATOR)
+        # reblog_content = status['reblog']['content']
+        # print(f"Reblog: {reblog_content}")
+        continue
+    if status['content']:
+        if STRING_TO_FIND in status['content']:
+            print(SEPARATOR)
+            print("<><><><><><><><><><><><><>")
+            print(f"Status: {status['content']}")
+            print(f"Reblogs: {status['reblogs_count']} Favorites: {status['favourites_count']}")
